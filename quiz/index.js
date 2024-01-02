7b1152ace55ffc10b545e2df237d2edf5ec7091c
jana <jana@123>
2024-01-02 22:05:20 +0200

    my project

---
+++ quiz/index.js
@@ -0,0 +1,41 @@
+const question=[
+    {
+        question : "which is the larger animal in the world",
+        answers: [
+            {text:"shrak" ,correct:false},
+            {text:"blue whale" ,correct:true},
+            {text:"Elephenet" ,correct:false},
+            {text:"Giraffe" ,correct:false},
+        ]
+    },
+    {
+        question : "which is the smallist country in the world ",
+        answers: [
+            {text:"vatican city" ,correct:false},
+            {text:"bhuthan" ,correct:true},
+            {text:"nepal" ,correct:false},
+            {text:"shri lanka" ,correct:false},
+        ]
+    },
+    {
+        question : "which is the largest desert in the world ",
+        answers: [
+            {text:"Kalahari" ,correct:false},
+            {text:"Gobi" ,correct:true},
+            {text:"sahara" ,correct:false},
+            {text:"antractica" ,correct:false},
+        ]
+    },
+    {
+        question : "which is the smalliest contains in the world ",
+        answers: [
+            {text:"asia" ,correct:false},
+            {text:"australia" ,correct:true},
+            {text:"arctic" ,correct:false},
+            {text:"africa" ,correct:false},
+        ]
+    }
+]
+const questionelement =document.getElementById("question")
+const anserbuttom =document.getElementById("answer-button")
+const nextbut =document.getElementById("next")
